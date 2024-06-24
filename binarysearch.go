7e609119037469a62e2f810fb14f481051928f0c
vlaD <sant.vladislav@gmail.com>
2024-06-25 00:36:00 +0300

    Initial commit

--- binarysearch.go
+++ binarysearch.go
@@ -13,9 +13,37 @@ func Search(list []int, value int) int {
 	}
 	if list[mid] > value {
 		return Search(list[:mid], value)
-	} else {
+	} else if list[mid] < value {
 		return Search(list[mid:], value)
 	}
+	return mid
+
+}
+
+func BubbleSort(list []int) []int {
+	for j := 0; j < len(list); j++ {
+		for i := 0; i < len(list)-1; i++ {
+			if list[i] > list[i+1] {
+				list[i], list[i+1] = list[i+1], list[i]
+			}
+
+		}
+	}
+
+	return list
+
+}
+func QuickSort(list []int) []int {
+	pivot := len(list) / 2
+	left := list[pivot:]
+	right := list[:pivot]
+	if len(left) > 1 {
+		QuickSort(left)
+	}
+	if len(right) > 1 {
+		QuickSort(right)
+	}
+	return list
 
 }
 
@@ -23,10 +51,16 @@ func (list List) BinarySearch(value int) int {
 	return Search(*list.listS, value)
 
 }
+func (list List) Sort() {
+	fmt.Println(BubbleSort(*list.listS), "Пузыречки")
+	fmt.Println(QuickSort(*list.listS), "Быстро")
+
+}
 
 func main() {
-	v := &[]int{1, 3, 5, 6, 8}
+	v := &[]int{111, 10, 1, 9, 5, 6, 8, 81}
 	testList := List{listS: v}
-	fmt.Print(testList.BinarySearch(5))
+	fmt.Print(testList.BinarySearch(5), " Поиск ", "\n")
+	testList.Sort()
 
 }
